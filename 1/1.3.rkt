8f8e9df69123c30694aea503298de8dbe67fba4e
lompdoctor <pleclair@protonmail.com>
2023-07-11 20:40:39 -0500

    Complete ex1.3 (1.2 already completed)

---
+++ 1/1.3.rkt
@@ -0,0 +1,10 @@
+#lang sicp
+(define (square x)
+  (* x x))
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+(define (greater-sum x y z)
+  (cond ((and (> x y) (> z y)) (sum-of-squares x z))
+        ((and (> y x) (> z x)) (sum-of-squares y z))
+        (else (sum-of-squares x y))))
+(greater-sum 1 3 4)
